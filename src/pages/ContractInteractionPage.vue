0c11e2bb82b0db1002d931b1fcf9f788dd852d1c
vichiti <vichiti369@gmail.com>
2025-03-27 05:25:11 +0000

    deplyed solidity program

--- src/pages/ContractInteractionPage.vue
+++ src/pages/ContractInteractionPage.vue
@@ -69,8 +69,32 @@ const web3Store = useWeb3Store()
 
 // Sample ABI for a simple storage contract
 const CONTRACT_ABI = [
-  "function setValue(uint256 _value)",
-  "function getValue() public view returns (uint256)"
+	{
+		"inputs": [],
+		"name": "getValue",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_value",
+				"type": "uint256"
+			}
+		],
+		"name": "setValue",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	}
 ]
 
 const contractAddress = ref('')
